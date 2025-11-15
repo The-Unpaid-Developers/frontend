b036bda944f92bc5fb034b6919c55a74b8684393
ju57_4_8361nn3r <xuan2103yyx@gmail.com>
2025-11-15 23:16:15 +0800

    feat: generate query

--- src/services/queryApi.ts
+++ src/services/queryApi.ts
@@ -38,3 +38,14 @@ export const deleteQueryAPI = async (queryName: string) => {
   const response = await axios.delete(`${API_BASE_URL}/${queryName}`);
   return response.data;
 };
+
+const mockGenerateQueryResponse = "[{\"$lookup\": {\"from\": \"lookups\",\"pipeline\": [{\"$match\": {\"_id\": \"techCompEOL\"}},{\"$unwind\": \"$data\"},{\"$replaceRoot\": {\"newRoot\": \"$data\"}}],\"as\": \"eolLookup\"}},{\"$unwind\":  {\"path\": \"$technologyComponents\",\"preserveNullAndEmptyArrays\": false}},{\"$addFields\": {\"matchingEOL\": {\"$filter\": {\"input\": \"$eolLookup\",\"as\": \"eol\",\"cond\": {\"$and\": [{\"$eq\": [\"$$eol.Product Name\",          \"$technologyComponents.productName\"]},{\"$eq\": [\"$$eol.Product Version\", \"$technologyComponents.productVersion\"]}]}}}}},{\"$match\": {\"matchingEOL\": {\"$ne\": []}}},{\"$unwind\": \"$matchingEOL\"},{\"$addFields\":  {\"eolDate\": {\"$dateFromString\": {\"dateString\": \"$matchingEOL.End-of-Life Date\",\"format\": \"%m/%d/%Y\"}}}},{\"$match\": {\"$expr\": {\"$and\": [{\"$lte\": [\"$eolDate\", {\"$dateAdd\": {\"startDate\": \"$$NOW\",  \"unit\": \"month\", \"amount\": 1}}]}]}}},{\"$group\": {\"_id\": \"$_id\",\"systemCode\": {\"$first\": \"$systemCode\"},\"documentState\": {\"$first\": \"$documentState\"},\"solutionOverview\": {\"$first\":  \"$solutionOverview\"},\"createdAt\": {\"$first\": \"$createdAt\"},\"lastModifiedAt\": {\"$first\": \"$lastModifiedAt\"},\"lastModifiedBy\": {\"$first\": \"$lastModifiedBy\"},\"expiringComponents\": {\"$push\": {\"componentId\":       \"$technologyComponents._id\",\"componentName\": \"$technologyComponents.componentName\",\"productName\": \"$technologyComponents.productName\",\"productVersion\": \"$technologyComponents.productVersion\",\"usage\":  \"$technologyComponents.usage\",\"eolDate\": \"$matchingEOL.End-of-Life Date\",\"adoptionStatus\": \"$matchingEOL.Adoption Status\",\"productCategory\": \"$matchingEOL.Product Category\"}}}},{\"$sort\": {\"systemCode\": 1}}]";
+
+export const generateQueryAPI = async (data: any) => {
+  // const response = await axios.post(
+  //   `${API_BASE_URL}/generate-query`,
+  //   data
+  // );
+  // return response.data;
+  return { query: mockGenerateQueryResponse };
+};
